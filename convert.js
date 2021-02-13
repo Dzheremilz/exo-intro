379832c489b1f7cda372b8eab9ef21a5c3f5cdf9
Dzheremilz <trempejeremie@gmail.com>
2021-02-13 14:33:00 +0100

    change number

--- convert.js
+++ convert.js
@@ -5,11 +5,11 @@ let sum = nb1 + nb2
 console.log(sum)
 
 let nb3 = 10
-let nb4 = Number('7')
+let nb4 = Number('8')
 sum = nb3 + nb4
 console.log(sum)
 
 let nb5 = 10
-let nb6 = '7'
+let nb6 = '9'
 sum = nb5 + +nb6    // +'1' converti '1' to 1
 console.log(sum)
